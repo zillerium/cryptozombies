d2fb9e6adf381e97e21d2aca729f7de9569378ac
Trevor <sales@merrows.co.uk>
2019-05-03 13:50:08 +0100

    feed

--- zombiefactory.sol
+++ zombiefactory.sol
@@ -1,10 +1,8 @@
-
 pragma solidity ^0.4.25;
 
 contract ZombieFactory {
 
-    // declare our event here
-    event NewZombie(uint id, string _name, uint _dna);
+    event NewZombie(uint zombieId, string name, uint dna);
 
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
@@ -16,11 +14,15 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
-    function _createZombie(string _name, uint _dna) private {
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
+
+    function _createZombie(string _name, uint _dna) internal {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
-        // and fire it here
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
-    } 
+    }
 
     function _generateRandomDna(string _str) private view returns (uint) {
         uint rand = uint(keccak256(abi.encodePacked(_str)));
@@ -28,7 +30,9 @@ contract ZombieFactory {
     }
 
     function createRandomZombie(string _name) public {
+        require(ownerZombieCount[msg.sender] == 0);
         uint randDna = _generateRandomDna(_name);
+        randDna = randDna - randDna % 100;
         _createZombie(_name, randDna);
     }
 
